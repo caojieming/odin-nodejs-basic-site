1b2e01c38b53110fca0f72240747ddcd463409c2
James Cao <jcao5360@gmail.com>
2026-06-15 18:07:59 -0400

    implemented via express

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-nodejs-basic-site
 Building a basic site using Node.js, the point of this "project" is to get a working nodejs server. Intentionally barebones so this can be easily used as a template for later projects.
+
+Update: now implemented with Express.
---
+++ app.js
@@ -0,0 +1,42 @@
+// load .env
+process.loadEnvFile();
+
+// import express
+const express = require("express");
+// builds filesystem-safe paths.
+const path = require("node:path");
+
+// our server
+const app = express();
+// set port number
+const PORT = process.env.PORT || 3000;
+
+
+// route received, what the server is GETting -> what the server responds with
+app.get("/", (req, res) => {
+  	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  	res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  	res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+	res.sendFile(path.join(__dirname, "404.html"));
+});
+
+
+// tell our server to listen for incoming requests on whatever port we specify, via localhost
+app.listen(PORT, (error) => {
+	// This is important!
+	// Without this, any startup errors will silently fail
+	// instead of giving you a helpful error message.
+	if (error) {
+		throw error;
+	}
+  	console.log(`listening on port ${PORT}`);
+});
